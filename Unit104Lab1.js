1108ccccc283480531505a0a48315d59cc99010e
MarianBarbarian <48567391+MarianBarbarian@users.noreply.github.com>
2019-04-18 18:43:21 -0600

    Add files via upload

---
+++ Unit104Lab1.js
@@ -0,0 +1,11 @@
+// array 
+
+let cupcakes=['strawberry cupcake', 'chocolate cupcake', 'vanilla cupcake'];
+let quantity=[10,30,40];
+
+let average=(quantity[0]+quantity[1]+quantity[2])/3;
+
+alert(cupcakes);
+alert(quantity);
+alert(average);
+
