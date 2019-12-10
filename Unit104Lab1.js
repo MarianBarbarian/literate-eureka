0ee4bb402475b2dc633b56422dd4157238c97cb9
MarianBarbarian <48567391+MarianBarbarian@users.noreply.github.com>
2019-12-10 09:56:13 -0700

    Update Unit104Lab1.js

--- Unit104Lab1.js
+++ Unit104Lab1.js
@@ -1,9 +1,9 @@
 // array 
 
-let cupcakes=['strawberry cupcake', 'chocolate cupcake', 'vanilla cupcake'];
-let quantity=[10,30,40];
+let cupcakes=['strawberry cupcake',' chocolate cupcake',' vanilla cupcake',' german chocolate cupcake',' tiramisu cupcake',' red velvet cupcake',' ube cupcake',' mint chocolate cupcake',' dulce de leche cupcake',' matcha cupcake'];
+let quantity=[10,30,40,10,23,22,55,40,60,12];
 
-let average=(quantity[0]+quantity[1]+quantity[2])/3;
+let average=(quantity[0]+quantity[1]+quantity[2]+quantity[3]+quantity[4]+quantity[5]+quantity[6]+quantity[7]+quantity[8]+quantity[9])/3;
 
 alert(cupcakes);
 alert(quantity);
